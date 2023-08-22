9e0cac7aa3405da867d08aa75e075f2dc4356419
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-21 20:21:01 -0500

    Finished Classes Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Hyundai";
+            myCar.Model = "Elantra";
+            myCar.Year = 2022;
+
+            var myTruck = new Car() { Make = "Chevy", Model = "S-10", Year = 1996 };
+
+            var myOldCar = new Car("Ford", "Probe", 1997);
+            
+            var carList = new List<Car>() {myCar, myTruck, myOldCar };
+
+            Console.WriteLine("  Car Make  || Car Model  || Vehicle Year  ");
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} || {vehicle.Model} || {vehicle.Year}");
+            }
+            
         }
     }
 }
